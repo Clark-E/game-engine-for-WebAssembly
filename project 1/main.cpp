c729e892d76d51dfbfe723cde8606d9453d74e5b
Ethan Clark <scratchcat2@sbcglobal.net>
2024-10-22 22:25:32 -0400

    added part 1

---
+++ project 1/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h> //for printf
+#include <iostream> //for std::cout
+
+//our main function.
+int main(int argc, char** argv){
+	
+	//print our messages
+	printf("Hello, sailor!\n");
+	
+	std::cout << "Hello, world!\n";
+	
+	//exit
+	return(0);
+	
+}
